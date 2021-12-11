443717dc89df578d1be37a5d2ae1a08870c95bbd
Marcos_Alexandre <junior.marcos.mjr@hotmail.com>
2021-12-11 07:30:10 -0300

    SellerForm

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,155 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable {
+	
+	//Dependência para o departamento (Dentro do nosso formulário poderemos adicionar e fazer o UpDate)
+    //Uma entidade relacionada ao formulário
+	private Seller entity;
+	
+	private SellerService service;
+	
+    //Com a implementação da interface as classes podem se inscrever para receber o evento da classe
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+
+	//Controles
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	//Instância do departamento
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+	
+	//Instância do SellerService
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+	
+	//
+	public void subscribeDataChangeListerner(DataChangeListener listerner) {
+		dataChangeListeners.add(listerner);
+	}
+	
+	//ação que permite o botão salvar ou fazer um UpDate
+	@FXML
+	public void onBtSaveAction(ActionEvent event) {
+		//Exceção para avisar ao programador que esqueceu a dependência
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		
+		try {
+			//Salvamos os dados digitados na aplicação na variável entity
+		    entity = getFormData();
+		    //Usamos o serviço para fazer o save ou Update
+		    service.saveOrUpdate(entity);
+		    //Utilizamos o método currentStage para fechar a cena quando uma ação ocorrer
+		    notifyDataChangeListeners();
+		    Utils.currentStage(event).close();
+		}catch(ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}catch(DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private void notifyDataChangeListeners() {
+		for(DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
+	}
+
+	//Método responsável por pegar as informações nas caixas de texto da aplicação
+	private Seller getFormData() {
+		//Criamos uma variável do tipo departamento
+		Seller obj = new Seller();
+		
+		ValidationException exception = new ValidationException("Validation error");
+		
+		//Passamos o Id de String para Integer usando o método tryParseToInt e guardamos no nosso objeto
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
+		  exception.addError("name","Field can't be empty");
+		}
+		//Pegamos o nome (Que já é do tipo String)
+		obj.setName(txtName.getText());
+		
+		if(exception.getErrors().size() > 0) {
+			throw exception;
+		}
+		
+		//retornamos o objeto
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		 Utils.currentStage(event).close();
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
+	//Método para pegar os dados do departamento e jogar nas caixas de texto
+	public void updateFormData() {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		//Pelo fato de termos usado uma caixa de texto, vamos ter que converter o Id para String
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+	}
+	
+	private void setErrorMessages(Map<String,String> errors) {
+		Set<String> fields = errors.keySet();
+		if(fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
+	}
+}
